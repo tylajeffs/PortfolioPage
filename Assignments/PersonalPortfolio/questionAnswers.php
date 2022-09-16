a886a7214be2252179dd20ce183060630f35d601
Tyla Jeffs <tylajeffs@gmail.com>
2022-09-16 09:27:27 -1000

    add questions to the assignment questions page

---
+++ Assignments/PersonalPortfolio/questionAnswers.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tyla Jeffs Portfolio</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="portfolio.css">
+</head>
+
+<body>
+
+    <header>
+
+
+
+    </header>
+    
+    <nav>
+        <ul id="navbar">
+            <li class="navv"><a href="default.asp">Home</a></li>
+            <li class="navv"><a href="news.asp">News</a></li>
+            <li class="navv"><a href="contact.asp">Contact</a></li>
+        </ul>
+    
+    </nav>
+
+    <br><br>
+
+
+    <main>
+
+        <h1>Assignment Questions</h1>
+        <br><br><br>
+
+        <h3>Question 1:</h3><br>
+        <p>Often times, users don't fill out the form completely correct, for example they might mistype their password/confirmation. What would you do to A) figure out that the information is not correct before inserting it into the database, and B) when it isn't correct would would you have to do to get the user to fix it? More specifically, how will you create a form that preserves the information they typed in and print out the errors?</p>
+        <br>
+        <h3>Answer:</h3>
+        <p>Here is the answer</p>
+        <br><br>
+
+        <h3>Question 2:</h3><br>
+        <p>How would you create PHP pages to edit the user's account (change name, email, or password)? How would you populate the form with the current information from the database? How would you keep track of what user account you are changing?</p>
+        <br>
+        <h3>Answer:</h3>
+        <p>Here is the answer</p>
+        <br><br>
+
+        <h3>Question 3:</h3><br>
+        <p>What tools do you have to be able to remember if a user has logged in? What is the difference between cookies and sessions.</p>
+        <br>
+        <h3>Answer:</h3>
+        <p>Here is the answer</p>
+        <br><br>
+
+        <h3>Question 4:</h3><br>
+        <p>How did the videos log someone in?</p>
+        <br>
+        <h3>Answer:</h3>
+        <p>Here is the answer</p>
+        <br><br>
+ 
+    </main>
+
+    <footer>
+
+    </footer>
+
+
+</body>
+
+
+
+
+
+</html>
